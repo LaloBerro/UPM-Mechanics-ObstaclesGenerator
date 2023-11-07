29e152598581c30a05fa5ca0bbfd1423c5be0797
Lalo <laurencioberro@hotmail.com>
2023-11-07 19:36:45 -0300

    [A] Added Installers for EmptyObstacleSpawner

---
+++ Runtime/EmptyObstaclesGenerator/Installers/EmptyObstacleSpawnerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using MechanicsObstaclesGenerator.Core.Domain;
+using MechanicsObstaclesGenerator.Core.Installers;
+using Spawners.Runtime.Core.Domain;
+using Spawners.Runtime.EmptySpawner.Domain;
+
+namespace EmptyObstaclesGenerator.Domain
+{
+    public class EmptyObstacleSpawnerInstaller : ObstacleSpawnerZinstaller<EmptyData>
+    {
+        protected override IObstacleSpawner GetObstacleSpawner(ISpawner<EmptyData> spawner)
+        {
+            return new EmptyObstacleSpawner(spawner);
+        }
+    }
+}
